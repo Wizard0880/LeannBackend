5d22f4b770449b348d54ec59f941d35553d4f950
Suryansh <suryansh.dxt.18@gmail.com>
2024-07-08 22:40:52 +0530

    github data added

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000
+const githubData = 
+	{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" },
+						{ "id": "1003", "type": "Blueberry" },
+						{ "id": "1004", "type": "Devil's Food" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5007", "type": "Powdered Sugar" },
+				{ "id": "5006", "type": "Chocolate with Sprinkles" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -19,6 +46,9 @@ app.get('/login',(req,res)=>{
     res.send("<h2>Login Successful</h2>")
 })
 
+app.get('/data',(req,res)=>{
+    res.json(githubData)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
